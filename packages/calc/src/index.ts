3f6280bd1e3fd7dd178d0e64f8a825e4f2ca9554
Greg Sterndale <gsterndale@gmail.com>
2023-05-25 08:25:55 -0400

    Begin implementing calculation

--- packages/calc/src/index.ts
+++ packages/calc/src/index.ts
@@ -1,5 +1,140 @@
-function sum(a: number, b: number): number {
-  return a + b;
+type Organization = {
+  newShareClass: string;
+  preMoneyValuation: number;
+  newMoneyRaised: number;
+  noteConversion: boolean;
+  notesConverttoNewClass: boolean;
+  expandOptionPool: boolean;
+  postMoneyOptionPoolSize: number;
+  notes: Note[];
+  preMoneyShareClasses: { name: string; numberOfShares: number }[];
+};
+
+type Note = {
+  principalInvested?: number;
+  interestStartDate?: Date;
+  interestRate?: number;
+  interestConverts?: boolean;
+  conversionCap: number;
+  conversionDiscount: number;
+  conversionDate: Date;
+};
+
+interface CapTableInterface {
+  readonly shareClasses: ShareClass[];
+  readonly totalPreMoneyShares: number;
+  readonly totalPreMoneyPercentOwnership: number;
+  readonly totalPreMoneyOwnershipValue: number;
+  readonly totalPostMoneyShares: number;
+  readonly totalPostMoneyNewOwnership: number;
+  readonly totalPostMoneyPercentChange: number;
+  readonly totalPostMoneyOwnershipValue: number;
+  readonly totalPostMoneyValueChange: number;
+  readonly totalPostMoneyDilution: number;
 }
 
-export { sum };
+type ShareClass = {
+  name: string; // found in Organization for most ShareClasses, other are "Convertible Notes into New Share Class" & "New Money Equity"
+  preMoneyShares: number; // found in Organization for most ShareClasses, calculated for "Convertible Notes into New Share Class" & "New Money Equity"
+  preMoneyPercentOwnership: number;
+  preMoneyOwnershipValue: number;
+  postMoneyShares: number;
+  postMoneyNewOwnership: number;
+  postMoneyPercentChange: number;
+  postMoneyOwnershipValue: number;
+  postMoneyValueChange: number;
+  postMoneyDilution: number;
+};
+
+class CapTable implements CapTableInterface {
+  // TODO why do we have to redefine these properties if they're defined in the CapTable interface already?
+  shareClasses!: ShareClass[];
+  totalPreMoneyShares!: number;
+  totalPreMoneyPercentOwnership!: number;
+  totalPreMoneyOwnershipValue!: number;
+  totalPostMoneyShares!: number;
+  totalPostMoneyNewOwnership!: number;
+  totalPostMoneyPercentChange!: number;
+  totalPostMoneyOwnershipValue!: number;
+  totalPostMoneyValueChange!: number;
+  totalPostMoneyDilution!: number;
+
+  organization: Organization;
+
+  constructor(org: Organization) {
+    this.organization = org;
+    this.calculate();
+  }
+
+  calculate() {
+    this.shareClasses = this.calcShareClasses();
+    this.totalPreMoneyShares = this.calcTotalPreMoneyShares();
+    this.totalPreMoneyPercentOwnership = 0;
+    this.totalPreMoneyOwnershipValue = 0;
+    this.totalPostMoneyShares = 0;
+    this.totalPostMoneyNewOwnership = 0;
+    this.totalPostMoneyPercentChange = 0;
+    this.totalPostMoneyOwnershipValue = 0;
+    this.totalPostMoneyValueChange = 0;
+    this.totalPostMoneyDilution = 0;
+  }
+
+  calcShareClasses() {
+    let shareClasses = this.organization.preMoneyShareClasses.map(
+      (input: { name: string; numberOfShares: number }) => {
+        return this.buildPreMoneyShareClass(input.name, input.numberOfShares);
+      }
+    );
+    shareClasses.push(
+      this.buildPostMoneyShareClass(
+        "Convertible Notes Into New Share Class",
+        0
+      ),
+      this.buildPostMoneyShareClass("New Money Equity", 0),
+      this.buildPostMoneyShareClass("New Options for Pool", 0)
+    );
+    return shareClasses;
+  }
+
+  calcTotalPreMoneyShares() {
+    return this.organization.preMoneyShareClasses
+      .map((input: { name: string; numberOfShares: number }) => {
+        return input.numberOfShares;
+      })
+      .reduce((total: number, numberOfShares: number) => {
+        return total + numberOfShares;
+      }, 0);
+  }
+
+  buildPreMoneyShareClass(name: string, numberOfShares: number): ShareClass {
+    return {
+      name: name,
+      preMoneyShares: numberOfShares,
+      preMoneyPercentOwnership: 0,
+      preMoneyOwnershipValue: 0,
+      postMoneyShares: 0,
+      postMoneyNewOwnership: 0,
+      postMoneyPercentChange: 0,
+      postMoneyOwnershipValue: 0,
+      postMoneyValueChange: 0,
+      postMoneyDilution: 0,
+    };
+  }
+
+  buildPostMoneyShareClass(name: string, numberOfShares: number): ShareClass {
+    return {
+      name: name,
+      preMoneyShares: 0,
+      preMoneyPercentOwnership: 0,
+      preMoneyOwnershipValue: 0,
+      postMoneyShares: numberOfShares,
+      postMoneyNewOwnership: 0,
+      postMoneyPercentChange: 0,
+      postMoneyOwnershipValue: 0,
+      postMoneyValueChange: 0,
+      postMoneyDilution: 0,
+    };
+  }
+}
+
+export { CapTable };
--- packages/calc/test/calc.test.ts
+++ packages/calc/test/calc.test.ts
@@ -1,11 +1,37 @@
 import { describe, expect, test } from "@jest/globals";
-import { sum } from "../src/index";
+import { CapTable } from "../src/index";
 
-describe("sum of two numbers", () => {
-  let a = 123;
-  let b = 456;
+describe("building a capitalization table", () => {
+  const org = {
+    newShareClass: "Series A",
+    preMoneyValuation: 30000000,
+    newMoneyRaised: 1000000,
+    noteConversion: true,
+    notesConverttoNewClass: true,
+    expandOptionPool: true,
+    postMoneyOptionPoolSize: 0.2,
+    notes: [
+      {
+        conversionCap: 5000000,
+        conversionDiscount: 0.2,
+        conversionDate: new Date(),
+      },
+    ],
+    preMoneyShareClasses: [
+      { name: "Founders' Shares", numberOfShares: 8000000 },
+      { name: "Rest of Common", numberOfShares: 1000000 },
+      { name: "Warrants", numberOfShares: 100000 },
+      { name: "Granted Options", numberOfShares: 800000 },
+      { name: "Options Available before", numberOfShares: 100000 },
+    ],
+  };
+  const table = new CapTable(org);
 
-  test("adds them together", () => {
-    expect(sum(a, b)).toBe(579);
+  test("has share classes", () => {
+    expect(table.shareClasses.length).toBeTruthy;
+  });
+
+  test("has total pre-money shares", () => {
+    expect(table.totalPreMoneyShares).toBe(10000000);
   });
 });
