2709dbfd980c73cfd964a08da2414041b001d870
gsterndale <gsterndale@gmail.com>
2023-07-13 13:12:36 +0000

    Deploying to gh-pages from @ gsterndale/capcalc@cf4803bf30760fa2b63f341584e862f894435b85 🚀

--- packages/calc/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Note, NoteFields } from "./Note";
-import Organization from "./Organization";
-import ShareClass from "./ShareClass";
-import CapTable from "./CapTable";
-
-export { Organization, CapTable, Note, NoteFields, ShareClass };
